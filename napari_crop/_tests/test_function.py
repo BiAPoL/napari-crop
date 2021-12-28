4cb4ab28f2159ffb2a7129669afa2db5c7d92002
Tim Morello <tdmorello@gmail.com>
2021-12-28 12:05:25 -0500

    Add tests for labels

--- napari_crop/_tests/test_function.py
+++ napari_crop/_tests/test_function.py
@@ -49,12 +49,12 @@ def test_crop_function_values_2d(make_napari_viewer, shape, shape_type, crop_exp
     assert np.array_equal(crop_expected, cropped_actual.data)
 
 
-image_data = [
-    [np.random.random((8, 8, 3)), True],  # 2d rgb
-    [np.random.random((8, 8, 4)), True],  # 2d rgba
-    [np.random.random((8, 8)), False],  # 2d
-    [np.random.random((8, 8, 8)), False],  # 3d
-    [np.random.random((8, 8, 8, 8)), False],  # 4d
+layer_data = [
+    [np.random.random((8, 8, 4)), True, "image"],  # 2d rgba
+    [np.random.random((8, 8)), False, "image"],  # 2d
+    [np.random.random((8, 8, 8)), False, "image"],  # 3d
+    [np.random.random((8, 8, 8, 8)), False, "image"],  # 4d
+    [np.arange(64).reshape(8, 8), False, "labels"],  # labels data
 ]
 shape_data = [
     np.array([[2, 2], [2, 5], [4, 5], [4, 2]]),  # 2x3 crop
@@ -63,26 +63,28 @@ shape_data = [
     np.array([[0, 2], [4, 4], [4, 2], [2, 0]]),  # diamond crop
 ]
 shape_types = ["rectangle", "rectangle", "rectangle", "polygon"]
-image_data_ids = ["2D_rgb", "2D_rgba", "2D", "3D", "4D"]
+layer_data_ids = ["2D_rgba", "2D", "3D", "4D", "labels"]
 shape_data_ids = ["2x3_crop", "neg_crop", "big_crop", "poly_crop"]
 
 
-@pytest.mark.parametrize("image_data,rgb", image_data, ids=image_data_ids)
+@pytest.mark.parametrize("layer_data,rgb,layer_type", layer_data, ids=layer_data_ids)
 @pytest.mark.parametrize(
     "shape_data,shape_type", zip(shape_data, shape_types), ids=shape_data_ids
 )
-def test_crop_function_nd(image_data, rgb, shape_data, shape_type, make_napari_viewer):
+def test_crop_function_nd(
+    layer_data, rgb, layer_type, shape_data, shape_type, make_napari_viewer
+):
     viewer = make_napari_viewer()
 
-    img_layer = viewer.add_image(image_data, rgb=rgb)
+    img_layer = viewer.add_image(layer_data, rgb=rgb)
 
     # shape data is (N,D) array where N is num verts and D is num dims
-    diff_dims = image_data.ndim - shape_data.shape[1]
+    diff_dims = layer_data.ndim - shape_data.shape[1]
     if rgb:
         diff_dims -= 1
     shape_data = np.insert(shape_data, [-1], np.zeros(diff_dims), axis=1)
-    shp_layer = viewer.add_shapes(shape_data, shape_type=shape_type)
+    shapes_layer = viewer.add_shapes(shape_data, shape_type=shape_type)
 
     nlayers = len(viewer.layers)
-    viewer.add_layer(crop_region(img_layer, shp_layer))
+    viewer.add_layer(crop_region(img_layer, shapes_layer))
     assert len(viewer.layers) == nlayers + 1
