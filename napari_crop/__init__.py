f0c2d6f352ab4f2d766243c8807d01c2dd7da12f
Marcelo Zoccoler <marcelozoc@gmail.com>
2025-04-07 16:53:00 +0200

    bump version

--- napari_crop/__init__.py
+++ napari_crop/__init__.py
@@ -1,6 +1,5 @@
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
-
-from ._dock_widgets import napari_experimental_provide_dock_widget
+from ._dock_widgets import CutWithPlane
 from ._function import crop_region, cut_with_plane
