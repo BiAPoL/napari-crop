072b624ec17175e7b0c120db3c305f81ec6e9389
zoccoler <marzoccoler@gmail.com>
2025-10-22 10:55:24 +0200

    Refactor project structure: migrate to pyproject.toml, remove setup.cfg, and update version handling in __init__.py. Drop support for older python versions.

--- napari_crop/__init__.py
+++ napari_crop/__init__.py
@@ -1,5 +1,7 @@
-
-__version__ = "0.1.10"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 from ._dock_widgets import CutWithPlane
 from ._function import crop_region, cut_with_plane, draw_fixed_shapes
